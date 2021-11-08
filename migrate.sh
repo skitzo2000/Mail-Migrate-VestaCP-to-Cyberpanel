f62e787abac7e7f93d3da12c8867dea2512ccc59
Skitzo <goldfish,paul@gmail.com>
2021-11-08 11:57:43 -0500

    Code Cleanup
    Code cleanup

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-if [ "$#" != "1" || ! -f "$pwd/migrate.conf"]; then echo "you must pass a .conf file or specify -init"
+if [ "$#" != "1" || ! -f "$pwd/migrate.conf"]; then echo "you must have a migrate.conf file or specify -init"
+else
+    if ["$1" = "-init" ];then 
+        if [-f "$pwd/migrate.conf_sample"];then cp migrate.conf_sample migrate.conf fi
+    else
+        curl https://raw.githubusercontent.com/skitzo2000/Mail-Migrate-VestaCP-to-Cyberpanel/main/migrate.conf_sample --output migrate.conf_sample
+        echo "migrate.conf_sample downlaoded.  Please adjust with your values for more info see the github page: https://github.com/skitzo2000/Mail-Migrate-VestaCP-to-Cyberpanel"
+    fi
 fi
 
-if ["$1" = "-init" ]; then cp migrate.conf_sample
-
-source $pwd/migrate.conf
+if [ -f "$pwd/migrate.conf" ]; then 
+    source $pwd/migrate.conf
+    echo "Init completed"
+fi
