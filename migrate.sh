affe60dd4cc61ac41298b69d489eeb944b8546ee
Skitzo <goldfish,paul@gmail.com>
2021-11-08 11:47:59 -0500

    fixes

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$#" != "1" || ! -f "$pwd/migrate.conf"]; then echo "you must pass a .conf file or specify -init"
 fi
-if ["$1" = "-init" ]; then 
+
+if ["$1" = "-init" ]; then cp migrate.conf_sample
 
 source $pwd/migrate.conf
